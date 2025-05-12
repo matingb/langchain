73f32dc299b783f2169a203fe9047504e80f914a
matias <mgarciaburgio@pods.com>
2025-05-12 12:45:36 -0300

    remove todo

--- backend/src/prompts.ts
+++ backend/src/prompts.ts
@@ -8,8 +8,6 @@ export const TONE_PROMPT = PromptTemplate.fromTemplate(
   "Rewrite it using the following tone(s): {tones}. Be strict about the tone. Ensure the output feels natural in the target language and culture. Ensure the output keeps the original meaning of the input",
 );
 
-// TODO setear TEMPERATURA o TOP_P COMO 0. Quiero algo deterministico. No se recomienda cambiar ambas
-// TODO set max length
 export const DETECT_LANGUAGE_PROMPT = PromptTemplate.fromTemplate(
   "Behave like a language expert system, detect the ISO 639-1 language code for the following text",
 );
